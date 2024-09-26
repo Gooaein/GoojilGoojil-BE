bb386c7bd647659e793f7e957427fada5d8f405b
MJJ <m3k0813@gmail.com>
2024-09-26 16:44:12 +0900

    feat: implement 현재 수강중인 Guest 조회

--- src/main/java/com/gooaein/goojilgoojil/controller/GuestController.java
+++ src/main/java/com/gooaein/goojilgoojil/controller/GuestController.java
@@ -1,13 +1,28 @@
 package com.gooaein.goojilgoojil.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.gooaein.goojilgoojil.dto.global.ResponseDto;
 import com.gooaein.goojilgoojil.service.GuestService;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
 @RestController
+@Tag(name = "손님 API", description = "손님 관련 API")
+@RequestMapping("/api/v1/rooms/{room_id}/")
 public class GuestController {
-    private final GuestService guestService;
+	private final GuestService guestService;
+
+	@Operation(summary = "방에 참가한 손님 전체조회", description = "현재 방에 참여하고 있는 손님의 id와 avatar를 전체 조회합니다.")
+	@GetMapping("/guests")
+	public ResponseDto<?> getGuests(@PathVariable("room_id") Long roomId) {
+		return ResponseDto.ok(guestService.getGuestsByRoomId(roomId));
+	}
 
 }
--- src/main/java/com/gooaein/goojilgoojil/service/GuestService.java
+++ src/main/java/com/gooaein/goojilgoojil/service/GuestService.java
@@ -1,15 +1,38 @@
 package com.gooaein.goojilgoojil.service;
 
-import com.gooaein.goojilgoojil.domain.Guest;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.gooaein.goojilgoojil.domain.Room;
+import com.gooaein.goojilgoojil.dto.response.GuestResponseDto;
 import com.gooaein.goojilgoojil.exception.CommonException;
 import com.gooaein.goojilgoojil.exception.ErrorCode;
 import com.gooaein.goojilgoojil.repository.GuestRepository;
+import com.gooaein.goojilgoojil.repository.RoomRepository;
+
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
 @Service
 public class GuestService {
-    private final GuestRepository guestRepository;
+	private final GuestRepository guestRepository;
+	private final RoomRepository roomRepository;
 
+	@Transactional(readOnly = true)
+	public List<GuestResponseDto> getGuestsByRoomId(Long roomId) {
+
+		Room room = getRoomByRoomId(roomId);
+
+		List<GuestResponseDto> guest = guestRepository.findGuestsByRoom(room);
+
+		return guest;
+	}
+
+	// 방 아이디로 방 조회
+	private Room getRoomByRoomId(Long roomId) {
+		return roomRepository.findById(roomId)
+			.orElseThrow(() -> new CommonException(ErrorCode.NOT_FOUND_ROOM));
+	}
 }
