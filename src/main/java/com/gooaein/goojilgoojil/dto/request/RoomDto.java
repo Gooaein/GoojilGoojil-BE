526191b9a1ba8a4e7b9184314b000791cdedbd92
dorkem <chlwogur1000@gmail.com>
2024-09-27 00:47:37 +0900

    refactor: 함수 수정, 인자 추가, 로컬 DB에서 subName이 Null로 되는 문제를 매핑으로 해결

--- src/main/java/com/gooaein/goojilgoojil/dto/request/RoomDto.java
+++ src/main/java/com/gooaein/goojilgoojil/dto/request/RoomDto.java
@@ -1,5 +1,6 @@
 package com.gooaein.goojilgoojil.dto.request;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.gooaein.goojilgoojil.domain.Room;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -11,16 +12,21 @@ import java.time.LocalDateTime;
 public class RoomDto {
     private Long id;
     private String name;
+
+    @JsonProperty("sub_name")
     private String subName;
+
     private LocalDateTime date;
     private String location;  // location 필드 추가
     private String url;
 
-    public RoomDto(String name, String subName, LocalDateTime date, String location) {
+    public RoomDto(Long id, String name, String subName, LocalDateTime date, String location, String url) {
+        this.id = id;
         this.name = name;
         this.subName = subName;
         this.date = date;
         this.location = location;
+        this.url = url;
     }
 
     public RoomDto(Long id, String url) {
@@ -29,13 +35,13 @@ public class RoomDto {
     }
 
     public static RoomDto from(Room room) {
-        RoomDto dto = new RoomDto();
-        dto.id = room.getId();
-        dto.name = room.getName();
-        dto.subName = room.getSubName();
-        dto.date = room.getDate();
-        dto.location = room.getLocation();
-        dto.url = room.getUrl();
-        return dto;
+        return new RoomDto(
+                room.getId(),
+                room.getName(),
+                room.getSubName(),
+                room.getDate(),
+                room.getLocation(),
+                room.getUrl()
+        );
     }
 }
