617c96e815422406dbfb69c0c0e6c5e7052ecb9d
dongkyeomjang <jangelliot0404@dgu.ac.kr>
2024-09-27 17:11:46 +0900

    fix: JsonProperty 추가

--- src/main/java/com/gooaein/goojilgoojil/dto/request/AvatarRequestDto.java
+++ src/main/java/com/gooaein/goojilgoojil/dto/request/AvatarRequestDto.java
@@ -1,6 +1,9 @@
 package com.gooaein.goojilgoojil.dto.request;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public record AvatarRequestDto(
+	@JsonProperty("avatar_base64")
 	String avatarBase64
 ) {
 }
