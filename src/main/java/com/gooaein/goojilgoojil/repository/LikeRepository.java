0e38887f0c2b327fca15198e375a67bddfbf1987
dongkyeomjang <jangelliot0404@dgu.ac.kr>
2024-09-29 02:07:52 +0900

    fix: 임시 유저 삭제 시, 연관관계있는 like의 userId 필드 null로 지정

--- src/main/java/com/gooaein/goojilgoojil/repository/LikeRepository.java
+++ src/main/java/com/gooaein/goojilgoojil/repository/LikeRepository.java
@@ -3,8 +3,10 @@ package com.gooaein.goojilgoojil.repository;
 import com.gooaein.goojilgoojil.domain.Like;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface LikeRepository extends JpaRepository<Like, Long> {
     Optional<Like> findByUserIdAndQuestionId(Long userId, String questionId);
+    List<Like> findAllByUserId(Long userId);
 }
